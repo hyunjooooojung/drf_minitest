b4a67be0e49e811840de3e3950190cccc42e262c
hyunjooooojung <hyunjoo3107@gmail.com>
2022-10-26 10:40:02 +0900

    클래스형 view 추가

--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,14 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.decorators import APIView
 from rest_framework.generics import get_object_or_404
 from articles import serializers
 from articles.models import Article
 from articles.serializers import ArticleSerializer
 
 # Create your views here.
+# 함수형
 @api_view(['GET', 'POST'])
 def index(request):
     if request.method == 'GET':
@@ -14,6 +16,22 @@ def index(request):
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
+        serializer = ArticleSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+# 클래스형
+class Article(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
         serializer = ArticleSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
