93d05b95c72c49083a95500f81978b4beeef6b7f
hyunjooooojung <hyunjoo3107@gmail.com>
2022-10-26 09:44:31 +0900

    init project , modelserializer로 간단한 GET/POST 응답 서버 만들기

---
+++ articles/views.py
@@ -0,0 +1,22 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from articles import serializers
+from articles.models import Article
+from articles.serializers import ArticleSerializer
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def index(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
